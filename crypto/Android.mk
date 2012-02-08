c642ac3a56765a59cd8c52e608f11c2b6cf61cc5
Kenny Root <kroot@google.com>
2012-02-07 16:29:55 -0800

    Add engines back to build
    The new keystore work requires OpenSSL engines to be enabled again.
    
    This adds back the Android.mk rules, tweaks the android-config.mk to
    build engine support, and adds in some essential files that were deleted
    during the subsequent merges since engine support was removed
    (crypto/engine/tb_asnmth.c and crypto/engine/tb_pkmeth.c)
    
    Change-Id: I7b8190d21f50c1dd1ed12bbd0961b6e543c18ecf

--- crypto/Android.mk
+++ crypto/Android.mk
@@ -249,6 +249,28 @@ local_src_files := \
 	ecdsa/ecs_ossl.c \
 	ecdsa/ecs_sign.c \
 	ecdsa/ecs_vrf.c \
+	engine/eng_all.c \
+	engine/eng_cnf.c \
+	engine/eng_ctrl.c \
+	engine/eng_dyn.c \
+	engine/eng_err.c \
+	engine/eng_fat.c \
+	engine/eng_init.c \
+	engine/eng_lib.c \
+	engine/eng_list.c \
+	engine/eng_pkey.c \
+	engine/eng_table.c \
+	engine/tb_asnmth.c \
+	engine/tb_cipher.c \
+	engine/tb_dh.c \
+	engine/tb_digest.c \
+	engine/tb_dsa.c \
+	engine/tb_ecdh.c \
+	engine/tb_ecdsa.c \
+	engine/tb_pkmeth.c \
+	engine/tb_rand.c \
+	engine/tb_rsa.c \
+	engine/tb_store.c \
 	err/err.c \
 	err/err_all.c \
 	err/err_prn.c \
