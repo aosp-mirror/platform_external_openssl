71c1fd3c45d9b91ddb2ad60db3b7983e092dd4e4
David 'Digit' Turner <digit@android.com>
2013-10-29 18:40:45 +0100

    Ensure iterating over a lhash table prevents unwanted resizes.
    This patch modifies the implementation of lhash tables to ensure
    that deleting or inserting items during a lh_doall or lh_doall_arg
    call will not unexpectedly resize the buckets array, leading to
    undefined behaviour.
    
    See http://crbug.com/298606 for an example.
    
    This is done by adding a new field ('iteration_state') to the
    lhash_st structure. Note that the field is placed at the end
    of the structure to preserve the OpenSSL ABI [1]
    
    This also removes the 'evil' changes to the 'down_load' field,
    which were performed to prevent this. Unfortunately, this didn't
    work due to an incorrect load-comparison computation is
    lh_delete() [2]
    
    [1] Since all _LHASH objects are heap-allocated through lh_new(),
        client code that accesses fields directly will not be
        impacted if their offsets do not change.
    
    [2] The original code used the following expression to determine
        when to contract the table during a lh_delete() operation:
    
           down_load >= (num_items * LOAD_MULT / num_nodes)
    
        Unfortunately, due to truncation towards zero, this could
        trigger even when down_load was set to 0 in certain cases.
        See the comments for the LH_SHOULD_CONTRACT() macro for
        more explanations.
    
    Change-Id: I9fa5b84c5d787d41fd520b63f7e641cf22fbb5b6

--- ssl/ssl_sess.c
+++ ssl/ssl_sess.c
@@ -999,11 +999,8 @@ void SSL_CTX_flush_sessions(SSL_CTX *s, long t)
 	if (tp.cache == NULL) return;
 	tp.time=t;
 	CRYPTO_w_lock(CRYPTO_LOCK_SSL_CTX);
-	i=CHECKED_LHASH_OF(SSL_SESSION, tp.cache)->down_load;
-	CHECKED_LHASH_OF(SSL_SESSION, tp.cache)->down_load=0;
 	lh_SSL_SESSION_doall_arg(tp.cache, LHASH_DOALL_ARG_FN(timeout),
 				 TIMEOUT_PARAM, &tp);
-	CHECKED_LHASH_OF(SSL_SESSION, tp.cache)->down_load=i;
 	CRYPTO_w_unlock(CRYPTO_LOCK_SSL_CTX);
 	}
 
