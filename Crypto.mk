1cbc4cda496558aff903f16309d2799f09798213
Tim Murray <timmurray@google.com>
2014-04-23 15:25:22 -0700

    Use -no-integrated-as on Darwin.
    Change-Id: Ib87eb36745de52c2765f8064be4b8ff9a497fb1e

--- Crypto.mk
+++ Crypto.mk
@@ -28,6 +28,10 @@ LOCAL_SHARED_LIBRARIES := $(log_shared_libraries)
 # in the NDK.
 ifeq (,$(TARGET_BUILD_APPS))
 LOCAL_CLANG := true
+ifeq ($(HOST_OS), darwin)
+LOCAL_ASFLAGS += -no-integrated-as
+LOCAL_CFLAGS += -no-integrated-as
+endif
 else
 LOCAL_SDK_VERSION := 9
 endif
